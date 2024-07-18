8223beece039e27bdb3b3f4e868975d8317c78e1
PedroH2003 <pedrohenriquecandidosilva14@gmail.com>
2024-07-18 17:50:27 -0300

    Addd password encoder and create ProfileEnum

--- src/main/java/com/pedro/todosimple/models/User.java
+++ src/main/java/com/pedro/todosimple/models/User.java
@@ -1,13 +1,20 @@
 package com.pedro.todosimple.models;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import com.pedro.todosimple.models.enums.ProfileEnum;
 
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -60,6 +67,21 @@ public class User {
     @OneToMany(mappedBy = "user")
     @JsonProperty(access = Access.WRITE_ONLY)
     private List<Task> tasks = new ArrayList<Task>();
+
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @CollectionTable(name = "user_profile")
+    @Column(name = "profile", nullable = false)
+    private Set<Integer> profiles = new HashSet<>();
+
+    public Set<ProfileEnum> getProfiles(){
+        return this.profiles.stream().map(x -> ProfileEnum.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public void addProfile(ProfileEnum profileEnum){
+        this.profiles.add(profileEnum.getCode());
+    }
 
 
 }
--- src/main/java/com/pedro/todosimple/services/UserService.java
+++ src/main/java/com/pedro/todosimple/services/UserService.java
@@ -1,12 +1,16 @@
 package com.pedro.todosimple.services;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.pedro.todosimple.models.User;
+import com.pedro.todosimple.models.enums.ProfileEnum;
 import com.pedro.todosimple.repositories.UserRepository;
 import com.pedro.todosimple.services.exceptions.DataBindingViolationException;
 import com.pedro.todosimple.services.exceptions.ObjectNotFoundException;
@@ -14,6 +18,9 @@ import com.pedro.todosimple.services.exceptions.ObjectNotFoundException;
 @Service
 public class UserService {
     
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Autowired
     private UserRepository userRepository;
 
@@ -30,6 +37,8 @@ public class UserService {
     @Transactional
     public User create(User obj) {
         obj.setId(null);
+        obj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
+        obj.setProfiles(Stream.of(ProfileEnum.USER.getCode()).collect(Collectors.toSet()));
         obj = this.userRepository.save(obj);
         return obj;
     }
@@ -38,6 +47,7 @@ public class UserService {
     public User update(User obj) {
         User newObj = findById(obj.getId());
         newObj.setPassword(obj.getPassword());
+        newObj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
         return this.userRepository.save(newObj);
     }
 
